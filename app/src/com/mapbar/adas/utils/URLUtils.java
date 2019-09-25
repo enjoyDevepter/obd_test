6899abb65db4ccfe1eaca5e5cd02e32a5f8d3a29
guomin <guomin@autoai.com>
2019-09-25 20:46:22 +0800

    chg 出厂设置完善

--- app/src/com/mapbar/adas/utils/URLUtils.java
+++ app/src/com/mapbar/adas/utils/URLUtils.java
@@ -1,14 +1,13 @@
 package com.mapbar.adas.utils;
 
+import com.miyuan.obd.BuildConfig;
+
 /**
  * Created by guomin on 2018/6/3.
  */
 
 public class URLUtils {
-    //    public static final String HOST = "http://box.iccm.cn/";
-//    public static final String HOST = "http://box.1668288.com/";
-    public static final String HOST = "http://47.92.101.179:8020/";
-//    public static final String HOST = "http://tpms.1668288.com/";
+    public static final String HOST = BuildConfig.HOST;
     public static final String ACTIVATE = HOST + "service/lisense/activationT";
     public static final String GETSN = HOST + "service/lisense/getSN";
     public static final String ACTIVATE_SUCCESS = HOST + "service/lisense/activationResult";
