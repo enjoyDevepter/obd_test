abfb71eb36c2b7e8447101d0b2b90a339bf511e5
guomin <guomin@autoai.com>
2019-09-25 20:44:23 +0800

    chg 代理商出厂设置完善

--- app/src/com/mapbar/adas/utils/URLUtils.java
+++ app/src/com/mapbar/adas/utils/URLUtils.java
@@ -1,13 +1,14 @@
 package com.mapbar.adas.utils;
 
+import com.miyuan.obd.BuildConfig;
+
 /**
  * Created by guomin on 2018/6/3.
  */
 
 public class URLUtils {
-    //    public static final String HOST = "http://box.iccm.cn/";
 //    public static final String HOST = "http://box.1668288.com/";
-    public static final String HOST = "http://47.92.101.179:8020/";
+//    public static final String HOST = "http://47.92.101.179:8020/";
 //    public static final String HOST = "http://tpms.1668288.com/";
-    public static final String RESET = HOST + "service/box/clearReturn";
+    public static final String RESET = BuildConfig.HOST + "service/box/clearReturn";
 }
